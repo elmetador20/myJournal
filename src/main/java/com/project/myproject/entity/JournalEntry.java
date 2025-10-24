bbca0387338d97b6e490d54540429df5818b71f1
sharique ahmad <ahmedsharique250@gmail.com>
2025-10-25 01:11:59 +0530

    fix

--- src/main/java/com/project/myproject/entity/JournalEntry.java
+++ src/main/java/com/project/myproject/entity/JournalEntry.java
@@ -6,47 +6,21 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import jakarta.persistence.Id;
-
-//pojo==pre old java object
+import lombok.Getter;
+import lombok.Setter;
+//entity me hm db ka data model(schema store krte hai)
+//pojo==plain old java object
 // @document spring ko ye batega ki  ye jo h wo mongodb ke collection se mapped entity hai
 @Document(collection="journal entries")
+@Getter
+@Setter
 public class JournalEntry {
   // @ID se hmne map kr di as a primary key jo age use kia hai Repository me
-  @Id 
+  @Id
   private ObjectId id;
   private String title;
   private String content;
   private LocalDateTime date;
-
- public void setDate(LocalDateTime date) {
-    this.date = date;
-  }
-
- public LocalDateTime getDate() {
-    return date;
-  }
-
- public ObjectId getId() {
-    return id;
-  }
- 
-  public void setId(ObjectId id) {
-    this.id = id;
-  }
-  
-  public String getTitle() {
-    return title;
-  }
-  public void setTitle(String title) {
-    this.title = title;
-  }
-  public void setContent(String content) {
-    this.content = content;
-  }
-  
-  public String getContent() {
-    return content;
-  }
 
 
 }
